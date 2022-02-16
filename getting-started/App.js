764045c65dd5fd94f343287fcddd06bb69c544eb
Artur Frimu <artur.frimu.ext@extendaretail.com>
2022-02-16 20:02:38 +0200

    feat-23: Add TextInput and Button

--- getting-started/App.js
+++ getting-started/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View>
+      <TextInput placeholder='Input your goals here' style={{borderColor: "black", borderWidth: 1, padding:10}}/>
+      <Button title={"Add"} />
+      </View>
     </View>
-  );
+  ); 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
