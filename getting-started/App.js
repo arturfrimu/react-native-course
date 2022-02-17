96aaab4ded04de1ed5627cd6870dcbbaca86385f
Artur Frimu <artur.frimu.ext@extendaretail.com>
2022-02-17 20:35:33 +0200

    feat-28: add useState and map() a list of goals, use StyleSheet

--- getting-started/App.js
+++ getting-started/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoal = () => {
+    setGoals((goals) => [...goals, enteredGoal]);
+    setEnteredGoal("");
+  };
+
+  const clearGoals = () => {
+    setGoals(() => []);
+  };
+
   return (
-    <View style={{padding: 50}}>
+    <View style={styles.screen}>
       <View>
-      <TextInput placeholder='Input your goals here' style={{borderColor: "black", borderWidth: 1, padding:10}}/>
-      <Button title={"Add"} />
+        <TextInput
+          placeholder="Input your goals here"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <View style={styles.actionsSection}>
+          <Button title={"Add"} onPress={() => addGoal()} />
+          <Button title={"Clear"} onPress={() => clearGoals()} />
+        </View>
+      </View>
+      <View>
+        {goals.map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))}
       </View>
     </View>
-  ); 
+  );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  textInput: { borderColor: "black", borderWidth: 1, padding: 10 },
+  actionsSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
